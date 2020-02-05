ca2f74101a225863397d7a76bf2e8eb4827bc344
kshriishwarya <46679711+kshriishwarya@users.noreply.github.com>
2020-02-05 08:16:36 +0530

    Update script.js

--- hb/script.js
+++ hb/script.js
@@ -1,57 +1,26 @@
+//Create your Partial 'user' here with html markup for displaying first_name, last_name, phone and email.
+Handlebars.registerPartial("user", "{{first_name}} {{last_name}} {{phone}} {{email}}" )
 
 
-    // var data = {
-    //  users : [{first_name:"Raj",last_name:"Kumar",phone:"9486794858",email:"shriandshri1999@gmail.com"},
-    //      {first_name:"Raj",last_name:"Kumar",phone:"9486794858",email:"shriandshri1999@gmail.com"}
-    
-    // ]
-
-
-    // }
-    
-    
-    
-      
-    //   //handlebar template html
-      
-    //  //var template=document.getElementById('template1').innerHTML;
-    //  var template = document.getElementById("template1").innerHTML;
-    //  //console.log(template)
-      
-    //   //handlebar's compile method returns special function which can be used to get final html
-    //   var compiledCode = Handlebars.compile(template);
-      
-    //  // json data is passed top compiled code and result will be html
-    //   var result = compiledCode(data);
-      
-    //  // compiled html can be rendered in document
-    //   $("#content").html(result);
-      
-      //document.getElementById('content').innerHTML=result;
-      Handlebars.registerPartial("user", "<h6>first_name:{{first_name}}  last_name:{{last_name}}  phone:{{phone}}  email:{{email}}<h6>" )
-       
-    var data = {
-        users : [{first_name:"Raj",last_name:"Kiran",phone:"9486794858",email:"shriandshri1999@gmail.com"},
+//Create your context with users array with first_name, last_name, phone and email.
+var context={
+     users : [{first_name:"Raj",last_name:"Kiran",phone:"9486794858",email:"shriandshri1999@gmail.com"},
             {first_name:"Selva",last_name:"Kumar",phone:"9486794238",email:"kshrishri1985@gmail.com"}]
-       
-       
-   
-   
-       }
-       
-         
-         //handlebar template html
-         
-        //var template=document.getElementById('template1').innerHTML;
-        var template = document.getElementById("template1").innerHTML;
-        //console.log(template)
-         
-         //handlebar's compile method returns special function which can be used to get final html
-         var compiledCode = Handlebars.compile(template);
-         
-        // json data is passed top compiled code and result will be html
-         var result = compiledCode(data);
-         
-       
-         document.getElementById('content').innerHTML=result;
-    
+  };
+
+window.onload = function(){
+  // Your code here
+
+    //retrieving the template `handlebars-template` from the HTML
+    var theTemplateScript = document.getElementById("handlebars-template").innerHTML;
+
+    //Compile the template using Handlebars.compile()
+    var theTemplate = Handlebars.compile(theTemplateScript);
+
+    //Pass the context as an argument to the compiled Template
+    var theCompiledHtml = theTemplate(context);
+
+    //insert the template-context package into '#content-placeholder'
+    document.getElementById('content-placeholder').innerHTML=theCompiledHtml
+
+};
