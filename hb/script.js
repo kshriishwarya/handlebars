12152617220f374cfd824e2e42819266a908f4e3
kshriishwarya <46679711+kshriishwarya@users.noreply.github.com>
2020-02-11 15:26:51 +0530

    Update script.js

--- hb/script.js
+++ hb/script.js
@@ -1,26 +1,13 @@
-//Create your Partial 'user' here with html markup for displaying first_name, last_name, phone and email.
-Handlebars.registerPartial("user", "{{first_name}} {{last_name}} {{phone}} {{email}}" )
 
+Handlebars.registerPartial("user","{{first_name}}");
 
-//Create your context with users array with first_name, last_name, phone and email.
 var context={
-     users : [{first_name:"Raj",last_name:"Kiran",phone:"9486794858",email:"shriandshri1999@gmail.com"},
-            {first_name:"Selva",last_name:"Kumar",phone:"9486794238",email:"kshrishri1985@gmail.com"}]
-  };
+users:[{first_name:"Raju",last_name:"kumar",phone:"9486794858"},{first_name:"Subash",last_name:"kumar",phone:"9486794858"}]
 
-window.onload = function(){
-  // Your code here
+}
+var rawTemplate=$('#watchTemplate').html()
 
-    //retrieving the template `handlebars-template` from the HTML
-    var theTemplateScript = document.getElementById("handlebars-template").innerHTML;
+var compiledTemplate = Handlebars.compile(rawTemplate);
+var ourGeneratedHTML = compiledTemplate(context);
+$('#content-holder').html(ourGeneratedHTML)
 
-    //Compile the template using Handlebars.compile()
-    var theTemplate = Handlebars.compile(theTemplateScript);
-
-    //Pass the context as an argument to the compiled Template
-    var theCompiledHtml = theTemplate(context);
-
-    //insert the template-context package into '#content-placeholder'
-    document.getElementById('content-placeholder').innerHTML=theCompiledHtml
-
-};
